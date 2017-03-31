93c54a7df19488867e2ad6966ca592d495a177f6
zpd <a88888a88888@qq.com>
2017-04-01 07:25:49 +0800

    !!!

--- src/org/zpd/base/picReg/Main.java
+++ src/org/zpd/base/picReg/Main.java
@@ -2,7 +2,7 @@ package org.zpd.base.picReg;
 
 public class Main {
 	public void main(String[] args){
-		String str = "mac eclipse fucking test!";
+		String str = "mac eclipse fucking test!!!";
 		System.out.println(str);
 	}
 }
